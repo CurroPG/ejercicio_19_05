2e475011d6dd4c95a82ac7ba1bde7d52d8151026
Curro Portillo <211curroportillo@gmail.com>
2025-10-28 17:36:40 +0100

    ejercicio 19

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura de la pirámide: "));
+        String relleno = System.console().readLine("Introduzca el carácter de relleno: ");        
+        int base = 1;
+        int longitudLinea = 1;
+        int espacios = altura-1;
+        
+        while (base <= altura) {        
+            for (int i = 1; i <= espacios; i++) {
+                System.out.print(" ");
+            }
+            for (int i = 1; i <= longitudLinea; i++) {
+                System.out.print(relleno);
+            }
+            System.out.println();
+            base++;
+            espacios--;
+            longitudLinea += 2;
+        }
     }
 }
